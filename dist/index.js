08a59736b46538e65ee0db277579b200b611e643
Ivan Bowen <bowenivan16@gmail.com>
2023-01-11 23:30:15 +0300

    classes shortcuts in ts

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,19 @@
 "use strict";
 console.log("TypeScript is amazing!");
 console.log("My dev skills are amazing");
+// class User {
+//    private email: string;
+//    name: string;
+//    age: number;
+//    city?: string;
+//    constructor(email: string, name: string, age: number) {
+//       this.email = email;
+//       this.name = name;
+//       this.age = age;
+//    }
+// }
 class User {
     constructor(email, name, age) {
-        this.city = "";
         this.email = email;
         this.name = name;
         this.age = age;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 console.log("TypeScript is amazing!");
 console.log("My dev skills are amazing");
 
+// class User {
+//    private email: string;
+//    name: string;
+//    age: number;
+//    city?: string;
+//    constructor(email: string, name: string, age: number) {
+//       this.email = email;
+//       this.name = name;
+//       this.age = age;
+//    }
+// }
+
 class User {
-   email: string;
-   name: string;
-   age: number;
-   city: string = "";
-   constructor(email: string, name: string, age: number) {
-      this.email = email;
-      this.name = name;
-      this.age = age;
+
+   city?: string;
+   constructor(
+      private email: string, 
+      public name: string, 
+      public age: number) {
+      
    }
 }
 
