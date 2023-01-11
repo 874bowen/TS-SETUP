c0573646d73c9d2a887962ccc4a684ee6a97ca44
Ivan Bowen <bowenivan16@gmail.com>
2023-01-12 00:04:51 +0300

    classes implementing interfaces

---
+++ dist/cam.js
@@ -0,0 +1,20 @@
+"use strict";
+// interfaces just means must have features
+class Instagram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class YouTube {
+    constructor(cameraMode, filter, burst, shorts) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.shorts = shorts;
+    }
+    createStory() {
+        console.log("Story is being created");
+    }
+}
